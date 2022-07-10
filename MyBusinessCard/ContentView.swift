1511bd6316ee4754e6bd3c876cef4a2ded759bfa
kenmaready <ken@maready.com>
2022-07-10 10:16:07 -0400

    Initial Commit

---
+++ MyBusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyBusinessCard
+//
+//  Created by Ken Maready on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyBusinessCard/MyBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBusinessCardApp.swift
+//  MyBusinessCard
+//
+//  Created by Ken Maready on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
